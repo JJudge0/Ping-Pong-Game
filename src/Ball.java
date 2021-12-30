df27a954dc73734794e1d8f0e535f63c23730a4b
JJ <73240114+JJudge0@users.noreply.github.com>
2021-12-30 14:51:16 +0000

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -6,37 +6,6 @@ import javax.swing.*;
 public class Ball extends Rectangle  {
     
 
-    // Random random;
-    // int xVelocity; //How fast the ball is moving on the x-axis
-    // int yVelocity; //How fast the ball is moving on the y-axis
-    // public  Ball()
-    // {
-    
-    
-        
-    // }
-
-    // public void Set_X_Direction(int Random_X_Direction ) 
-    // {
-            
-    // }
-
-    // public void Set_Y_Direction(int Random_Y_Direction) 
-    // {
-            
-    // }
-
-    // public void Move() 
-    // {
-            
-    // }
-
-    // public void Draw(Graphics g)
-    // {
-
-
-    // }
-
     Random random;
     int xVelocity;
 	int yVelocity;
@@ -68,7 +37,8 @@ public class Ball extends Rectangle  {
 		y += yVelocity;
 	}
 	public void draw(Graphics g) {
-		g.setColor( new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+		// g.setColor( new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255))); Note: // Multi colored ball DISABLED FOR NOW!
+		g.setColor(Color.GREEN);
 		g.fillOval(x, y, height, width);
 	}
 
