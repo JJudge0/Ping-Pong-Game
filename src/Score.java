fff3f5ec71f1ca428ce1d18e4726f31861a156b2
JJ <73240114+JJudge0@users.noreply.github.com>
2021-12-30 16:34:27 +0000

    Ping Pong Game
    Editing scores that are displayed on the screen

--- src/Score.java
+++ src/Score.java
@@ -17,7 +17,7 @@ public class Score extends Rectangle  {
     Score.GAME_WIDTH = GAME_WIDTH;
     Score.GAME_HEIGHT = GAME_HEIGHT;
     }
-    public void draw(Graphics g)  // Declaring 
+    public void draw(Graphics g)  
     {
     g.setColor(Color.WHITE);
     g.setFont(new Font("Consolas",Font.BOLD,60));
@@ -25,7 +25,7 @@ public class Score extends Rectangle  {
     g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT); // Draws the line in the middle of the game
     
     g.drawString(String.valueOf(Score_Player1/10)+String.valueOf(Score_Player1%10), (GAME_WIDTH/2)-100, 300); // Draws scores on the screen for Player 1 [Where '300' is put '50' this should display at the top of the screen]
-    g.drawString(String.valueOf(Score_Player2/10)+String.valueOf(Score_Player2%10), (GAME_WIDTH/2)+30, 590); // Draws scores on the screen for Player 2 [Where '300' is put '50' this should display at the top of the screen]
+    g.drawString(String.valueOf(Score_Player2/10)+String.valueOf(Score_Player2%10), (GAME_WIDTH/2)+30, 300); // Draws scores on the screen for Player 2 [Where '300' is put '50' this should display at the top of the screen]
     // PUT "%10" so it displays double digits    [300] is middle of the screen, [50] is top of the screen, [590] is for bottom of the screen
 }
 }
